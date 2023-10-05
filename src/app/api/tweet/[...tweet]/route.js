3025a1f411ca9a1b9435ef21e1a1f0055e39e9f2
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2023-10-05 14:20:31 +0500

    feat: Implement API endpoint to retrieve all tweets of a user
    - Create secure endpoint for fetching all tweets associated with a user with its userID

--- src/app/api/tweet/[...tweet]/route.js
+++ src/app/api/tweet/[...tweet]/route.js
@@ -54,3 +54,18 @@ export async function PUT(request, content) {
 	}
 }
 
+export async function GET(request, content) {
+	try {
+		await mongoose.connect(connectionStr);
+
+		// Extract user ID from the request parameters or body
+		const user_Id = content.params.tweet[0];
+
+		// Retrieve tweets of the user where isDeleted is false
+		const tweets = await Tweet.find({ userId: user_Id, isDeleted: false });
+
+		return NextResponse.json({ tweets }, { status: 200 });
+	} catch {
+		internalServerError();
+	}
+}
