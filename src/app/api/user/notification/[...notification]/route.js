d26b779747011016bc9b1f188355872c834197a2
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2023-10-13 12:30:00 +0500

    feat: Implement API endpoint to mark all notification as read
    - Create secure endpoint to update the read status of a notification
    - Implement validation for notification ID and user authentication
    - Enhance backend to support marking notifications as read via PUT requests

--- src/app/api/user/notification/[...notification]/route.js
+++ src/app/api/user/notification/[...notification]/route.js
@@ -35,3 +35,33 @@ export async function DELETE(request, content) {
 		internalServerError();
 	}
 }
+
+
+//	Api to open a notification for a user
+export async function PUT(request,content){
+	try {
+		await mongoose.connect(connectionStr);
+
+		// Extract notification ID from the request parameters or body
+		const  notificationId  = content.params.notification[0];
+
+		// Find the notification by ID
+		let notification = await Notification.findById(notificationId);
+
+		// Check if the notification exists
+		if (!notification) {
+			return notFoundError();
+		}
+
+		// Update fields to mark the notification as deleted
+		notification.isOpened = true;
+		notification.updatedAt = new Date().toISOString();
+
+		// Save the updated notification
+		await notification.save();
+
+		return NextResponse.json({ message:"Notification has been opened" }, { status: 200 });
+	} catch {
+		internalServerError();
+	}
+}
