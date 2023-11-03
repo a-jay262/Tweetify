130a7898919097d307c14b4e09ef4f641ec30e32
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2023-11-03 22:20:39 +0500

    fix: Changed the cloudinary key, secret
    - solved the configuration error

--- src/lib/cloudinaryConfig.js
+++ src/lib/cloudinaryConfig.js
@@ -1,8 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-	cloud_name: process.env.cloud_name,
 	api_key: process.env.api_key,
 	api_secret: process.env.api_secret,
+	cloud_name: process.env.cloud_name,
 });
-
