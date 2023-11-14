bd13857f186242317c6e33d2fef1eb72c35773b3
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2023-11-14 19:29:25 +0500

    feat: Implement API endpoint to mute a relationship using PUT method
    - Introduce a new API endpoint for muting relationships via the PUT method
    - Enhance backend functionality to support the muting of connections between users
    - Improve the application's capacity to manage and control user relationships

--- src/app/api/relation/[...relation]/route.ts
+++ src/app/api/relation/[...relation]/route.ts
@@ -41,3 +41,34 @@ export async function DELETE(
 		return internalServerError();
 	}
 }
+
+
+export async function PUT(request: NextRequest, content: Record<string, any>) {
+	try {
+		// Connect to the MongoDB database using the connection string
+		await mongoose.connect(connectionStr);
+
+		// Extract relation ID from the request parameters
+		const secondPersonId = content.params.relation[0];
+
+		// Find the relation by ID
+		const relation = await Relation.findOne({ secondPerson: secondPersonId });
+
+		// Check if the relation exists
+		if (!relation) {
+			return notFoundError();
+		}
+
+		// Update the relation to mute (set isMuted to true)
+		await relation.updateOne({ isMuted: true });
+
+		// Respond with a success message and a 200 status
+		return NextResponse.json(
+			{ message: 'Relation updated successfully to mute' },
+			{ status: 200 }
+		);
+	} catch (error) {
+		// Handle internal server error in case of an exception
+		return internalServerError();
+	}
+}
